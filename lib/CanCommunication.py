fbddde2896a0c19879ce3b10918c79e83ec690af
Elias Kotlyar <elias.kotlyar@gmail.com>
2020-04-20 17:01:20 +0200

    Test

--- lib/CanCommunication.py
+++ lib/CanCommunication.py
@@ -13,6 +13,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 class CanCommunication:
+
     """A simple example class"""
     led_message = can.Message(
         arbitration_id=0x545, data=[0, 0, 0, 0, 0xF0, 0, 0, 0], is_extended_id=False
@@ -30,12 +31,12 @@ class CanCommunication:
             self.bus = can.interface.Bus(channel="can0", bustype="socketcan")
 
         self.task_rpm = self.bus.send_periodic(self.rpm_message, 0.03)
-        #self.task_led = self.bus.send_periodic(self.led_message, 0.03)
+        self.task_led = self.bus.send_periodic(self.led_message, 0.03)
 
 
     def setValue(self, x):
         self.led_message.data[3] = int("0x" + str(x) + "0", 16)
-        self.led_message.data[4]
+        #self.led_message.data[4]
         self.task.modify_data(self.led_message)
         print(self.led_message)
 
@@ -49,28 +50,26 @@ class CanCommunication:
         self.setValue(0)
         self.task.stop()
 
-    def setRpm(self,rpm):
-        rpm = rpm * 6.4
+
+    def fuelconsumption(self,fuel):
+        fuel = self.toUint16(fuel)
+        self.led_message.data[1] = fuel[0]
+        self.led_message.data[2] = fuel[1]
+        self.task_rpm.modify_data(self.rpm_message)
+
+    def toUint16(self,rpm):
         rpm_uint16 = int(rpm)
         rpm_msb = rpm_uint16 & 0xff
         rpm_lsb = rpm_uint16 >> 8
+        return rpm_lsb,rpm_msb
 
 
-        self.rpm_message.data[3] = rpm_lsb
-        self.rpm_message.data[4] = rpm_msb
+    def setRpm(self,rpm):
+        rpm = rpm * 6.4
+        rpm = self.toUint16(rpm)
+        self.rpm_message.data[3] = rpm[0]
+        self.rpm_message.data[4] = rpm[1]
         self.task_rpm.modify_data(self.rpm_message)
-        #print(self.rpm_message)
-
-        #self.bus.send(self.rpm_message)
-
-        #self.led_message.data[3] = int("0x" + str(4) + "5", 16)
-        #self.task_led.modify_data(self.led_message)
-
-
-        #self.bus.send(self.led_message)
-
-
-        #self.task.modify_data(self.rpm_message)
 
 
     def stoptask(self):
