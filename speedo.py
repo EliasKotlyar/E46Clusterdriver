a17c695d5ca522e5efc34f1db7cd3c2c27af7b26
Elias Kotlyar <elias.kotlyar@gmail.com>
2020-04-25 21:02:35 +0200

    Speedo

--- speedo.py
+++ speedo.py
@@ -1,17 +1,27 @@
-import board
-import pulseio
+import Adafruit_BBIO.PWM as PWM
 import time
 
-
+# Setup Pins:
+# Enable PWM
+# config-pin P1_33 pwm
+# Enable 12V Rail:
+# config-pin P1_31 hi
 def map(x, in_min, in_max, out_min, out_max):
-    return int((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
 
 speed = 30
 SpeedSendVal = map(speed, 0, 250, 0, 1680)
-SpeedSendVal = 1000
+SpeedSendVal = 15000
 
-pin = board.P1_33
-print("TEST pin {0}".format(pin))
-pwm = pulseio.PWMOut( pin, duty_cycle=(2 ** 15), frequency=SpeedSendVal, variable_frequency=False)
-time.sleep(1000)
+pin = "P1_33"
+PWM.stop(pin)
+PWM.cleanup()
+# PWM.start(channel, duty, freq=2000, polarity=0)
+# duty values are valid 0 (off) to 100 (on)
+PWM.start(pin, 50, SpeedSendVal, 0)
+# PWM.set_duty_cycle(pin, 25.5)
+
+time.sleep(60)
+PWM.stop(pin)
+PWM.cleanup()
